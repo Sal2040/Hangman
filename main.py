6a237f6075a5ee245a8067694d5e5b4f8f7c34db
Samer Alwan <alwan.samer24@gmail.com>
2023-06-07 22:16:34 +0200

    language support added

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 from helpers import WordList, Word, Player, Game
-template_folder = "templates"
+from jinja2 import Environment, FileSystemLoader
+from jinja2.exceptions import TemplateNotFound
+from argparse import ArgumentParser
+import os
+import sys
+
 
 def main():
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("--language", default="CS", required=False)
+
+    args = arg_parser.parse_args()
+    language = args.language
+    language_u = language.upper()
+
+    template_folder = os.path.join("templates", language_u)
+
+    try:
+        env = Environment(loader=FileSystemLoader(template_folder))
+        yes = env.get_template("yes.txt")
+        play_again = env.get_template("play_again.txt")
+    except TemplateNotFound:
+        try:
+            language_l = language.lower()
+            template_folder = os.path.join("templates", language_l)
+            env = Environment(loader=FileSystemLoader(template_folder))
+            yes = env.get_template("yes.txt")
+            play_again = env.get_template("play_again.txt")
+        except TemplateNotFound as e:
+            sys.exit(f"Language '{language}' not supported.")
+
     wordlist = WordList(text_file="ceska_podstatna_jmena_test.txt")
-    play_again = "A"
-    while play_again == "A":
+    repeat_game = yes.render()
+    while repeat_game == yes.render():
         word = Word(wordlist.get_random_word())
         player = Player(lives=10)
         game = Game(word=word, player=player, template_folder=template_folder)
         game.play()
-        play_again = input("Hrát znovu?[A/N]")
+        repeat_game = input(play_again.render())
 
 
 if __name__=="__main__":
