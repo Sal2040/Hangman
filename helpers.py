e969c41e1146d983d0f64e486b3bd0f53e7c81f6
Samer Alwan <alwan.samer24@gmail.com>
2023-06-07 17:39:45 +0200

    templates added to guess_letter()

--- helpers.py
+++ helpers.py
@@ -41,14 +41,17 @@ class Word:
     def guess_letter(self, player, input_letter):
         letter = input_letter.lower()
         if letter in self.__word:
+            # correct guess
             if letter not in self.__correctly_guessed_letters:
                 self.__correctly_guessed_letters.add(input_letter)
-                print("Správně!")
+                return 0
             else:
-                print("Tohle písmeno jsi již uhodl.")
+                # correct but repeated guess
+                return 1
         else:
+            # wrong guess
             player.remove_life()
-            print("Špatně :(")
+            return 2
 
     def is_completed(self):
         if len(set(self.__word)) == len(self.__correctly_guessed_letters):
@@ -74,23 +77,6 @@ class Player:
             return False
 
 
-# class Decliner:
-#     def __init__(self):
-#         self.pismeno = defaultdict(lambda: "písmen")
-#         self.pismeno[1] = "písmeno"
-#         for i in range(2, 5):
-#             self.pismeno[i] = "písmena"
-#
-#         self.zivot = defaultdict(lambda: "životů")
-#         self.zivot[1] = "život"
-#         for i in range(2, 5):
-#             self.zivot[i] = "životy"
-#
-#         self.zbyvat = defaultdict(lambda: "Zbývá")
-#         for i in range(2, 5):
-#             self.zbyvat[i] = "Zbývají"
-
-
 class Game:
     def __init__(self, word, player, template_folder):
         self.__word = word
@@ -101,19 +87,13 @@ class Game:
         self.__guess_letter = self.__env.get_template('guess_letter.txt')
         self.__remaining_lives = self.__env.get_template('remaining_lives.txt')
         self.__outro = self.__env.get_template('outro.txt')
+        self.__answer_eval = self.__env.get_template('answer_eval.txt')
 
     def play(self):
-#        print("Vítej ve hře Šibenice. Pokud chceš pokračovat, stiskni ENTER...")
         print(self.__introduction.render())
         input()
-        # print(
-        #     f"Hledané slovo má {self.__word.return_word_len()} {self.__decliner.pismeno[self.__word.return_word_len()]}.",
-        #     f"{self.__decliner.zbyvat[self.__player.return_lives()]} ti {self.__player.return_lives()}"
-        #     f" {self.__decliner.zivot[self.__player.return_lives()]}.",  # jak to zalomit?
-        #     "Začínáme!",
-        # )
         context = {
-            "world_len": self.__word.return_word_len(),
+            "word_len": self.__word.return_word_len(),
             "lives": self.__player.return_lives()
         }
         print(self.__first_input.render(context))
@@ -121,20 +101,16 @@ class Game:
 
         while self.__player.is_alive() and not self.__word.is_completed():
             input_letter = input(self.__guess_letter.render())
-            self.__word.guess_letter(player=self.__player, input_letter=input_letter)
-            # print(
-            #     self.__word.return_correctly_guessed_part(),
-            #     "\n",
-            #     f"{self.__decliner.zbyvat[self.__player.return_lives()]} ti {self.__player.return_lives()} {self.__decliner.zivot[self.__player.return_lives()]}.",  # jak to zalomit?
-            # )
+            result = self.__word.guess_letter(player=self.__player, input_letter=input_letter)
+            context = {
+                "result": result
+            }
+            print(self.__answer_eval.render(context))
             print(self.__word.return_correctly_guessed_part())
-            context = {"lives": self.__player.return_lives()}
+            context = {
+                "lives": self.__player.return_lives()
+            }
             print(self.__remaining_lives.render(context))
-
-        # if self.__player.is_alive():
-        #     print("Gratulace, vyhrál jsi! \n")
-        # else:
-        #     print("Tentokrát ti no nevyšlo... \n", f'To slovo mělo být "{self.__word}"')
 
         context = {
             "alive": self.__player.is_alive(),
