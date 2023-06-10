b9261c00355315ccf2b900f90cca05fc23a6c088
Samer Alwan <alwan.samer24@gmail.com>
2023-06-10 17:29:23 +0200

    GameSession created

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 import random
 from jinja2 import Environment, FileSystemLoader
-from enum import Enum
+import os
+import sys
+from jinja2.exceptions import TemplateNotFound
 
 
 class WordList:
@@ -71,42 +73,101 @@ class Player:
 
 
 class Game:
-    def __init__(self, word, player, template_folder):
+    def __init__(self, word: Word, player: Player, templates: dict):
         self.__word = word
         self.__player = player
-        self.__env = Environment(loader=FileSystemLoader(template_folder))
-        self.__introduction = self.__env.get_template('introduction.txt')
-        self.__first_input = self.__env.get_template('first_input.txt')
-        self.__guess_letter = self.__env.get_template('guess_letter.txt')
-        self.__remaining_lives = self.__env.get_template('remaining_lives.txt')
-        self.__outro = self.__env.get_template('outro.txt')
-        self.__answer_eval = self.__env.get_template('answer_eval.txt')
+        self.__templates = templates
 
     def play(self):
-        print(self.__introduction.render())
+        print(self.__templates["introduction"].render())
         input()
         context = {
             "word_len": self.__word.return_word_len(),
             "lives": self.__player.return_lives()
         }
-        print(self.__first_input.render(context))
+        print(self.__templates["first_input"].render(context))
         print(self.__word.return_correctly_guessed_part())
 
         while self.__player.is_alive() and not self.__word.is_completed():
-            input_letter = input(self.__guess_letter.render())
+            input_letter = input(self.__templates["guess_letter"].render())
             result = self.__word.guess_letter(player=self.__player, input_letter=input_letter)
             context = {
                 "result": result
             }
-            print(self.__answer_eval.render(context))
+            print(self.__templates["answer_eval"].render(context))
             print(self.__word.return_correctly_guessed_part())
             context = {
                 "lives": self.__player.return_lives()
             }
-            print(self.__remaining_lives.render(context))
+            print(self.__templates["remaining_lives"].render(context))
 
         context = {
             "alive": self.__player.is_alive(),
             "word": self.__word
         }
-        print(self.__outro.render(context))
+        print(self.__templates["outro"].render(context))
+
+
+class GameSession:
+    def __init__(self, language_folder, language):
+        self.__wordlist, self.__templates = GameSession.fetch_language(language_folder, language)
+
+    @staticmethod
+    def fetch_templates(template_folder_path):
+        env = Environment(loader=FileSystemLoader(template_folder_path))
+        templates = {
+            "introduction": env.get_template('introduction.txt'),
+            "first_input": env.get_template('first_input.txt'),
+            "guess_letter": env.get_template('guess_letter.txt'),
+            "answer_eval": env.get_template('answer_eval.txt'),
+            "remaining_lives": env.get_template('remaining_lives.txt'),
+            "outro": env.get_template('outro.txt'),
+            "yes": env.get_template("yes.txt"),
+            "no": env.get_template("no.txt"),
+            "play_again": env.get_template("play_again.txt")
+                }
+        return templates
+
+    @staticmethod
+    def fetch_language(language_folder, language):
+        language_u = language.upper()
+        language_l = language.lower()
+
+        template_folder_path = os.path.join(language_folder, language_u, "templates")
+        language_file = language_u + ".txt"
+        language_file_path = os.path.join(language_folder, language_u, language_file)
+
+        try:
+            templates = GameSession.fetch_templates(template_folder_path)
+        except TemplateNotFound:
+            try:
+                template_folder_path = os.path.join(language_folder, language_l, "templates")
+                templates = GameSession.fetch_templates(template_folder_path)
+            except TemplateNotFound:
+                sys.exit(f"Language '{language}' not supported. Templates are missing")
+
+        try:
+            wordlist = WordList(text_file=language_file_path)
+        except FileNotFoundError:
+            try:
+                language_file = language_l + ".txt"
+                language_file_path = os.path.join(language_folder, language_l, language_file)
+                wordlist = WordList(text_file=language_file_path)
+            except FileNotFoundError:
+                sys.exit(f"Language '{language}' not supported. Corresponding list of words is missing.")
+
+        return wordlist, templates
+
+    def game_loop(self, lives):
+        yes = self.__templates["yes"].render()
+        no = self.__templates["no"].render()
+        play_again = self.__templates["play_again"].render()
+        repeat_game = yes
+        while repeat_game == yes:
+            word = Word(self.__wordlist.get_random_word())
+            player = Player(lives=lives)
+            game = Game(word=word, player=player, templates=self.__templates)
+            game.play()
+            repeat_game = input(play_again)
+            while repeat_game not in [yes, no]:
+                repeat_game = input(play_again)
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from helpers import WordList, Word, Player, Game
-from jinja2 import Environment, FileSystemLoader
-from jinja2.exceptions import TemplateNotFound
+from helpers import GameSession
 from argparse import ArgumentParser
-import os
-import sys
 
 
 def main():
@@ -14,50 +10,10 @@ def main():
     args = arg_parser.parse_args()
     lives = args.lives
     language = args.language
-    language_u = language.upper()
+    language_folder = "languages"
 
-    template_folder = "templates"
-    template_folder_path = os.path.join(template_folder, language_u)
-    language_folder = "word_lists"
-    language_file = language_u + ".txt"
-    language_file_path = os.path.join(language_folder, language_file)
-
-    try:
-        env = Environment(loader=FileSystemLoader(template_folder_path))
-        yes = env.get_template("yes.txt")
-        no = env.get_template("no.txt")
-        play_again = env.get_template("play_again.txt")
-    except TemplateNotFound:
-        try:
-            language_l = language.lower()
-            template_folder_path = os.path.join(template_folder, language_l)
-            env = Environment(loader=FileSystemLoader(template_folder_path))
-            yes = env.get_template("yes.txt")
-            no = env.get_template("no.txt")
-            play_again = env.get_template("play_again.txt")
-        except TemplateNotFound:
-            sys.exit(f"Language '{language}' not supported. Templates are missing")
-
-    try:
-        wordlist = WordList(text_file=language_file_path)
-    except FileNotFoundError:
-        try:
-            language_l = language.lower()
-            language_file = language_l + ".txt"
-            language_file_path = os.path.join(language_folder, language_file)
-            wordlist = WordList(text_file=language_file_path)
-        except FileNotFoundError:
-            sys.exit(f"Language '{language}' not supported. Corresponding list of words is missing.")
-
-    repeat_game = yes.render()
-    while repeat_game == yes.render():
-        word = Word(wordlist.get_random_word())
-        player = Player(lives=lives)
-        game = Game(word=word, player=player, template_folder=template_folder_path)
-        game.play()
-        repeat_game = input(play_again.render())
-        while repeat_game not in [yes.render(), no.render()]:
-            repeat_game = input(play_again.render())
+    session = GameSession(language_folder, language)
+    session.game_loop(lives)
 
 
 if __name__ == "__main__":
